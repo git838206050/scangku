141a2e1f007adeff8ab67739276b4f50588eb0aa
lcy <838206050@qq.com>
2018-03-16 17:03:57 +0800

    添加购物车页面

--- html/backstage/main/js/showproduct.js
+++ html/backstage/main/js/showproduct.js
@@ -1,17 +1,15 @@
 $(function(){
 	var productJsonInfo = [
-	          { id:1, name: 'Runoob1' , url: 'aaa.com1'},
-	          { id:2, name: 'Google2' , url: 'bbb.com2'},
-	          { id:3, name: 'Google3' , url: 'bbb.com3'},
-	          { id:4, name: 'Taobao4' , url: 'ccc.com4'},
-	          { id:5, name: 'Google5' , url: 'bbb.com5'},
-	          { id:6, name: 'Taobao6' , url: 'ccc.com6'},
-	          { id:7, name: 'Google7' , url: 'bbb.com7'},
-	          { id:8, name: 'Taobao8' , url: 'ccc.com8'},
-	          { id:9, name: 'Google9' , url: 'bbb.com9'},
-	          { id:10, name: 'Taobao10' , url: 'ccc.com10'},
-	          { id:11, name: 'Taobao11' , url: 'ccc.com11'},
-	          { id:12, name: 'Taobao12' , url: 'ccc.com12'}
+	          { id:1, name: 'Runoob1' , url: 'https://f10.baidu.com/it/u=2903580660,3473125105&fm=72'},
+	          { id:2, name: 'Google2' , url: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3994450565,4064214859&fm=27&gp=0.jpg'},
+	          { id:3, name: 'Google3' , url: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1891820928,1620033395&fm=27&gp=0.jpg'},
+	          { id:4, name: 'Taobao4' , url: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=18558777,2970581122&fm=27&gp=0.jpg'},
+	          { id:5, name: 'Google5' , url: 'http://img3.imgtn.bdimg.com/it/u=3762564554,919211873&fm=200&gp=0.jpg'},
+	          { id:6, name: 'Taobao6' , url: 'http://img1.imgtn.bdimg.com/it/u=3400877733,3849922232&fm=200&gp=0.jpg'},
+	          { id:7, name: 'Google7' , url: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1307334686,2078374065&fm=27&gp=0.jpg'},
+	          { id:8, name: 'Taobao8' , url: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3935222401,1983125872&fm=27&gp=0.jpg'},
+	          { id:9, name: 'Google9' , url: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2280183197,763913268&fm=200&gp=0.jpg'},
+	          { id:10, name: 'Taobao10' , url: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3994450565,4064214859&fm=27&gp=0.jpg'}
 	        ];
 	new Vue({
 		el: '#app',
--- html/frontdesk/base/js/base.js
+++ html/frontdesk/base/js/base.js
@@ -1,11 +1,72 @@
 $(function () {
-    var h=window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-    var ajaxActionHeight = $(document).height()-100;
-    var ajaxActionBL = false;
-    $(document).scroll(function() {
-        if($(document).scrollTop()+h >= ajaxActionHeight && !ajaxActionBL){
-            ajaxActionBL = true;
-            alert("是时候加载了");
+    /**
+     * 注意：该方法未封装完成，只针对购物车描述。
+     * 描述字符溢出处理
+     * boxWidth:显示文字的面积宽度
+     * boxHeight:显示文字的面积高度
+     * $column1:显示文字的标签
+     */
+    if($('.describe').length > 0){
+        var boxWidth = $('.describe-box').width();
+        var boxHeight = $('.cart-item-img').height()-44;
+        var $column1 = $('.describe');
+        for(var i=0; i<$column1.length; i++){
+            var text = $column1.eq(i).text();
+            var column1MB = $column1.eq(i).css("margin-bottom");
+            var column1PB = $column1.eq(i).css("padding-bottom");
+            var boxArea = boxWidth * boxHeight;
+            var fontArea = parseInt($column1.eq(i).css("font-size")) * parseInt($column1.eq(i).css("font-size"));
+            var fontMaxNum = parseInt(boxArea/fontArea * 1.3);
+            $column1.eq(i).css("height","auto");
+            $column1.eq(i).text("隰");
+            if($column1.eq(i).height() < boxHeight){
+                $column1.eq(i).text(text);
+                if($column1.eq(i).text().length > fontMaxNum){
+                    text = text.substring(0,fontMaxNum);
+                }
+                var test2 = "";
+                while ($column1.eq(i).height() > boxHeight){
+                    test2 = text.substring(0,text.length-1);
+                    $column1.eq(i).text(test2);
+                    text = test2;
+                }
+                if(test2 != ""){
+                    text = text.substring(0,text.length-2);
+                    $column1.eq(i).text(text+"...");
+                }
+            }
+            $column1.eq(i).css("margin-bottom",column1MB);
+            $column1.eq(i).css("padding-bottom",column1PB);
         }
-    });
+    }
 });
+
+window.onload=function (){
+    itemLoading();
+
+    /**
+     *  用于滚动条划到一定程度时加载更多商品
+     *  由于Vue加载数据会延迟DOM加载的原因，该方法需要放在window加载完毕后执行
+     *  参数maxHeight：滚动条距离最低部多少距离时触发发送Ajax请求事件（单位：px）
+     */
+    function itemLoading(maxHeight){
+        if(undefined === maxHeight){
+            maxHeight = 100;
+        }
+        var h=window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        var ajaxActionHeight = $(document).height()-maxHeight;
+        /**
+         * 是否允许触发Ajax请求
+         * 指定时间内只能发送一次请求，否则滚动条滑动到那段值时会重复复发请求
+         * true表示是
+         * @type {boolean}
+         */
+        var ajaxActionBL = false;
+        $(document).scroll(function() {
+            if($(document).scrollTop()+h >= ajaxActionHeight && !ajaxActionBL){
+                ajaxActionBL = true;
+                console.log("是时候加载了");
+            }
+        });
+    };
+};
